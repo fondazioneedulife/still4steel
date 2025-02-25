c899d07a91b310799a5a950e7fddf7a53918b299
queen-git-hub <queensleyk10@gmail.com>
2025-02-25 09:44:10 +0100

    Inserimento routes e ho messo gli stili globali

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AggiungiProdotti from '../pagine/AggiungiProdotti';
+import Magazzino from '../pagine/Magazzino'; 
+import SecondaSottopagina from '../pagine/SecondaSottopagina';
+import TerzaSottopagina from '../pagine/TerzaSottopagina';
+import Riepilogo from '../pagine/Riepilogo'
+import Navbar from '../componenti/Navbar';
+import QuintaSottopagina from '../pagine/QuintaSottopagina';
+import NavFooter from '../componenti/NavFooter'
+import VisualizaProdotti from '../pagine/VisuaizzaProdotti'
+import DettagliSpedizioni from '../pagine/DettagliSpedizione'
+function App() {
+    return (
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" />
+          <Route path="/magazzino" element= {<Magazzino/>}/>
+          <Route path="/aggiungi-prodotti" element={<AggiungiProdotti />} />
+          <Route path="/seconda-sottopagina" element={<SecondaSottopagina />} />
+          <Route path="/terza-sottopagina" element={<TerzaSottopagina />} />
+          <Route path="/riepilogo" element={<Riepilogo />} />
+          <Route path="/quinta-sottopagina" element={<QuintaSottopagina />} />
+          <Route path='/visualizza-prodotti' element={<VisualizaProdotti/>} />
+          <Route path="/dettagli-spedizione" element={<DettagliSpedizioni />}/>
+        </Routes>
+        <NavFooter/>
+      </Router>
+    );
+  }
+  
+  
+
+
+export default App;
