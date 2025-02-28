299889c220e75b52ad014c0d8c1ae3bf107656df
giulymezz <ggiulia.mmazzi@gmail.com>
2025-02-28 11:28:37 +0100

    Fix db

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,15 +1,19 @@
-module.exports = {
+export default {
     development: {
       client: 'pg',
       connection: {
-        host: process.env.DB_HOST || 'still4steel',
+        host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'postgres',
         password: process.env.DB_PASSWORD || 'postgres',
         database: process.env.DB_NAME || 'memodb',
+        port: process.env.DB_PORT || 5432,
       },
       migrations: {
-        tableName: 'knex_migrations',
+        directory: './migrations'
       },
+      seeds: {
+        directory: './seeds'
+      }
     },
   };
   
