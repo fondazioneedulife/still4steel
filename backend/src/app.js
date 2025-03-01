6bee6111f08a2a2ce714df10354bbdce59c48e4f
alenico04 <19131@studenti.marconiverona.edu.it>
2025-03-01 15:41:19 +0100

    update warehouse

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./docs/swagger.js";
 import { router as authRoutes } from "./routes/auth.js"; // Import con estensione .js
 import companiesRoutes from "./routes/companies.js";
+import warehousesRoutes from "./routes/warehouses.js";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,8 @@ app.use("/api/auth", authRoutes);
 // CRUD companies
 app.use("/api/companies", companiesRoutes);
 
+// CRUD warehouse
+app.use("/api/warehouses", warehousesRoutes);
 
 // Documentazione Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
